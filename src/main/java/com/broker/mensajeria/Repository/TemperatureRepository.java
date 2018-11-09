47647be6247a91558dc5d5a5cbd3841428fad0d5
Juan Joa <juandanieljoa@gmail.com>
2018-11-09 19:59:23 -0400

    clean out repository

--- src/main/java/com/broker/mensajeria/Repository/TemperatureRepository.java
+++
@@ -1,6 +0,0 @@
-package com.broker.mensajeria.Repository;
-
-import com.broker.mensajeria.Classes.Temperaturetransaction;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TemperatureRepository extends MongoRepository<Temperaturetransaction, String> {}
